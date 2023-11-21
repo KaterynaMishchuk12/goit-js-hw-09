600e7df90ff52e0acf1bb9e61c4f5499caa705dd
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2023-11-21 11:51:01 +0200

    1st task done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const bodyEl = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const PROMPT_DELAY = 1000;
+let timerId = null;
+
+startButton.addEventListener('click', onStartBtnClick);
+stopButton.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, PROMPT_DELAY);
+  startButton.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
