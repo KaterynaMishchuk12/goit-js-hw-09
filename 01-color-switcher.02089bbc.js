966b2136178ded8d45aeff2ee95739aa0cdf8653
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2023-11-21 10:02:18 +0000

    Deploying to gh-pages from @ KaterynaMishchuk12/goit-js-hw-09@600e7df90ff52e0acf1bb9e61c4f5499caa705dd 🚀

---
+++ 01-color-switcher.02089bbc.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let a=null;e.addEventListener("click",(function(){a=setInterval((()=>{t.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.02089bbc.js.map
